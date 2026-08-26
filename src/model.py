d93f0e4194064329814a4ecd3265a94b243b0429
Sumukh Prasad <sumukhprasad@icloud.com>
2026-08-26 13:28:34 +0530

    Added model files.

---
+++ src/model.py
@@ -0,0 +1,115 @@
+# First model implementation
+# using a small barebones UNet isntead of pretrained model.
+# roughly:
+# image --> Dataset --> DataLoader --> UNet --> prediction --> bin. mask -> polygon
+
+
+import torch
+import torch.nn as nn
+
+
+class DoubleConv(nn.Module):
+	"""
+	Two consecutive 3x3 double convolutions, each followed by BatchNorm and ReLU.
+	"""
+
+	def __init__(self, in_channels, out_channels):
+		super().__init__()
+
+		self.block = nn.Sequential(
+			nn.Conv2d(
+				in_channels, out_channels,
+				kernel_size=3,
+				padding=1,
+				bias=False
+			),
+			nn.BatchNorm2d(out_channels),
+			nn.ReLU(inplace=True),
+			
+			nn.Conv2d(
+				out_channels, out_channels,
+				kernel_size=3,
+				padding=1,
+				bias=False
+			),
+			nn.BatchNorm2d(out_channels),
+			nn.ReLU(inplace=True)
+		)
+
+	def forward(self, x):
+		return self.block(x)
+
+
+
+class UNet(nn.Module):
+	def __init__(self, in_channels=1, out_channels=1):
+		super().__init__()
+
+		# encoding
+		self.enc1 = DoubleConv(in_channels, 64)
+		self.enc2 = DoubleConv(64, 128)
+		self.enc3 = DoubleConv(128, 256)
+		self.enc4 = DoubleConv(256, 512)
+
+		self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+		# bottleneck
+		self.bottleneck = DoubleConv(512, 1024)
+
+		# decoding
+		self.up4 = nn.ConvTranspose2d(
+			1024, 512, kernel_size=2, stride=2
+		)
+		self.dec4 = DoubleConv(1024, 512)
+
+		self.up3 = nn.ConvTranspose2d(
+			512, 256, kernel_size=2, stride=2
+		)
+		self.dec3 = DoubleConv(512, 256)
+
+		self.up2 = nn.ConvTranspose2d(
+			256, 128, kernel_size=2, stride=2
+		)
+		self.dec2 = DoubleConv(256, 128)
+
+		self.up1 = nn.ConvTranspose2d(
+			128, 64, kernel_size=2, stride=2
+		)
+		self.dec1 = DoubleConv(128, 64)
+
+		# final 1x1 convolution
+		self.output = nn.Conv2d(
+			64,
+			out_channels,
+			kernel_size=1,
+		)
+
+	def forward(self, x):
+
+		# encoding
+		e1 = self.enc1(x)
+		e2 = self.enc2(self.pool(e1))
+		e3 = self.enc3(self.pool(e2))
+		e4 = self.enc4(self.pool(e3))
+
+		# bottleneck
+		b = self.bottleneck(self.pool(e4))
+
+		# decoding
+		d4 = self.up4(b)
+		d4 = torch.cat([d4, e4], dim=1)
+		d4 = self.dec4(d4)
+
+		d3 = self.up3(d4)
+		d3 = torch.cat([d3, e3], dim=1)
+		d3 = self.dec3(d3)
+
+		d2 = self.up2(d3)
+		d2 = torch.cat([d2, e2], dim=1)
+		d2 = self.dec2(d2)
+
+		d1 = self.up1(d2)
+		d1 = torch.cat([d1, e1], dim=1)
+		d1 = self.dec1(d1)
+
+		return self.output(d1)
